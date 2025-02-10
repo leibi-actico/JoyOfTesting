1d23fa34ee5300eba7892d44d5d8719280624393
Andreas Leibrock <andreas.leibrock@actico.com>
2025-02-10 16:36:27 +0100

    jacoco verification

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,13 @@
+import com.adarshr.gradle.testlogger.TestLoggerExtension
+
 plugins {
     java
     id("org.springframework.boot") version "3.4.2"
     id("io.spring.dependency-management") version "1.1.7"
+    id("jacoco")
+    id("org.sonarqube") version "6.0.1.5171"
+    id("com.adarshr.test-logger") version "4.0.0"
+
 }
 
 group = "com.actico.architecture.testing"
@@ -32,6 +38,54 @@ dependencies {
     testImplementation("org.testcontainers:junit-jupiter")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 }
+
+
+tasks.jacocoTestReport {
+    reports {
+        xml.required = true
+    }
+}
+
+
+tasks.jacocoTestCoverageVerification {
+    violationRules {
+        rule {
+            element = "CLASS"
+            limit {
+                counter = "LINE"
+                value = "COVEREDRATIO"
+                minimum = BigDecimal.valueOf(0.95)
+                    }
+            excludes = listOf("com.actico.architecture.testing.joyoftesting.JoyOfTestingApplication")
+        }
+
+    }
+}
+
+tasks.check {
+    dependsOn(tasks.jacocoTestCoverageVerification)
+}
+
+apply(plugin = "com.adarshr.test-logger")
+configure<TestLoggerExtension> {
+    showExceptions = true
+    showStackTraces = true
+    showFullStackTraces = false
+    showCauses = true
+    slowThreshold = 2000
+    showSummary = true
+    showSimpleNames = false
+    showPassed = true
+    showSkipped = true
+    showFailed = true
+    showOnlySlow = false
+    showStandardStreams = false
+    showPassedStandardStreams = true
+    showSkippedStandardStreams = true
+    showFailedStandardStreams = true
+    logLevel = LogLevel.LIFECYCLE
+}
+
 
 tasks.withType<Test> {
     useJUnitPlatform()
--- src/main/java/com/actico/architecture/testing/joyoftesting/JoyOfTestingApplication.java
+++ src/main/java/com/actico/architecture/testing/joyoftesting/JoyOfTestingApplication.java
@@ -9,5 +9,4 @@ public class JoyOfTestingApplication {
     public static void main(String[] args) {
         SpringApplication.run(JoyOfTestingApplication.class, args);
     }
-
 }
