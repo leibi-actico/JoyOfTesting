53b1ce04dbb27a48ed10770677c82e5ba67907b8
Andreas Leibrock <andreas.leibrock@actico.com>
2025-02-20 17:56:51 +0100

    betterTestNames

--- src/test/java/com/actico/architecture/testing/joyoftesting/switchhandling/SwitchHandlingTest.java
+++ src/test/java/com/actico/architecture/testing/joyoftesting/switchhandling/SwitchHandlingTest.java
@@ -6,31 +6,36 @@ import org.junit.jupiter.params.provider.EnumSource;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.assertj.core.api.Fail.fail;
 
 class SwitchHandlingTest {
 
 
     @Test
-    void oldSchool() {
+    void enumValueOnShouldReturnSwitchOn() {
         assertThat(SwitchHandling.oldSchool(SwitchHandling.Switch.ON)).isEqualTo("Switch is ON");
+    }
+
+    @Test
+    void enumValueOffShouldReturnSwitchOff() {
         assertThat(SwitchHandling.oldSchool(SwitchHandling.Switch.OFF)).isEqualTo("Switch is OFF");
     }
 
 
     @ParameterizedTest
     @EnumSource(SwitchHandling.Switch.class)
-    void oldSchool(SwitchHandling.Switch s) {
+    void allEnumValuesShouldBeTested(SwitchHandling.Switch s) {
         if (s == SwitchHandling.Switch.ON) {
             assertThat(SwitchHandling.oldSchool(s)).isEqualTo("Switch is ON");
         } else if (s == SwitchHandling.Switch.OFF) {
             assertThat(SwitchHandling.oldSchool(s)).isEqualTo("Switch is OFF");
         } else {
-            assertThat(SwitchHandling.oldSchool(s)).isEqualTo("Switch is in unknown state");
+            fail("Unknown enum value: " + s);
         }
     }
 
     @Test
-    void testNull() {
+    void nullEnumValue_whenPassedAsArgumentToOldSchool_shouldRaiseNullpointerexception() {
         assertThatThrownBy(() -> SwitchHandling.oldSchool(null))
                 .isInstanceOf(NullPointerException.class)
                 .hasMessage("Cannot invoke \"com.actico.architecture.testing.joyoftesting.switchhandling.SwitchHandling$Switch.ordinal()\" because \"s\" is null");
