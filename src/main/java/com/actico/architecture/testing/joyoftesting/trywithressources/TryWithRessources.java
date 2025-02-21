3f30c26906ac7a1595ab704b4b064e527a5edba6
Andreas Leibrock <andreas.leibrock@actico.com>
2025-02-21 15:52:36 +0100

    SOC try-with-ressources

--- src/main/java/com/actico/architecture/testing/joyoftesting/trywithressources/TryWithRessources.java
+++ src/main/java/com/actico/architecture/testing/joyoftesting/trywithressources/TryWithRessources.java
@@ -1,17 +1,18 @@
 package com.actico.architecture.testing.joyoftesting.trywithressources;
 
 import lombok.NoArgsConstructor;
+import lombok.extern.log4j.Log4j2;
 
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
-import java.net.URI;
 import java.nio.file.Files;
 import java.nio.file.LinkOption;
 import java.nio.file.Path;
 import java.util.Optional;
 
 @NoArgsConstructor
+@Log4j2
 public class TryWithRessources {
     public Optional<String> readFirstLineFromFile(String path) {
         try (BufferedReader br = new BufferedReader(new FileReader(path))) {
@@ -22,15 +23,10 @@ public class TryWithRessources {
     }
 
     public Optional<String> readFirstLineFromFileWithExplicitCheckBeforeTheTry(String path) {
-        var realPath = Path.of(path);
-        boolean exists = Files.exists(realPath, LinkOption.NOFOLLOW_LINKS);
-        if (exists) {
-            try (BufferedReader br = new BufferedReader(new FileReader(path))) {
-                return Optional.of(br.readLine());
-            } catch (IOException e) {
-                return Optional.empty();
-            }
+        if (!Files.exists(Path.of(path), LinkOption.NOFOLLOW_LINKS)) {
+            log.warn("Maybe we want to tell the user that the file does not exist");
+            return Optional.empty();
         }
-        return Optional.empty();
+        return readFirstLineFromFile(path);
     }
 }
